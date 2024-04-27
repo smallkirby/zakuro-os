63cd4676d2c731a6802dbd83d339612e142bc464
smallkirby <ssmallkirby@gmail.com>
2024-04-27 23:34:59 +0900

    loader: get EFI memory map
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- ZakuroLoaderPkg/Main.c
+++ ZakuroLoaderPkg/Main.c
@@ -1,9 +1,147 @@
+#include <Library/PrintLib.h>
+#include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
+#include <Protocol/BlockIo2.h>
+#include <Protocol/DiskIo.h>
+#include <Protocol/LoadedImage.h>
+#include <Protocol/SimpleFileSystem.h>
 #include <Uefi.h>
+
+/// Thin wrapper struct for UEFI memory map.
+struct MemoryMap {
+  UINTN buffer_size;
+  VOID *buffer;
+  UINTN map_size;
+  /// Used to check the map is up-to-date.
+  UINTN map_key;
+  UINTN descriptor_size;
+  UINT32 descriptor_version;
+};
+
+/// Get UEFI memory map.
+EFI_STATUS GetMemoryMap(struct MemoryMap *map) {
+  if (map->buffer == NULL) {
+    return EFI_BUFFER_TOO_SMALL;
+  }
+
+  map->map_size = map->buffer_size;
+  return gBS->GetMemoryMap(&map->map_size, (EFI_MEMORY_DESCRIPTOR *)map->buffer,
+                           &map->map_key, &map->descriptor_size,
+                           &map->descriptor_version);
+}
+
+/// Get printable string for UEFI memory type.
+const CHAR16 *GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
+  switch (type) {
+    case EfiReservedMemoryType:
+      return L"EfiReservedMemoryType";
+    case EfiLoaderCode:
+      return L"EfiLoaderCode";
+    case EfiLoaderData:
+      return L"EfiLoaderData";
+    case EfiBootServicesCode:
+      return L"EfiBootServicesCode";
+    case EfiBootServicesData:
+      return L"EfiBootServicesData";
+    case EfiRuntimeServicesCode:
+      return L"EfiRuntimeServicesCode";
+    case EfiRuntimeServicesData:
+      return L"EfiRuntimeServicesData";
+    case EfiConventionalMemory:
+      return L"EfiConventionalMemory";
+    case EfiUnusableMemory:
+      return L"EfiUnusableMemory";
+    case EfiACPIReclaimMemory:
+      return L"EfiACPIReclaimMemory";
+    case EfiACPIMemoryNVS:
+      return L"EfiACPIMemoryNVS";
+    case EfiMemoryMappedIO:
+      return L"EfiMemoryMappedIO";
+    case EfiMemoryMappedIOPortSpace:
+      return L"EfiMemoryMappedIOPortSpace";
+    case EfiPalCode:
+      return L"EfiPalCode";
+    case EfiPersistentMemory:
+      return L"EfiPersistentMemory";
+    case EfiMaxMemoryType:
+      return L"EfiMaxMemoryType";
+    default:
+      return L"InvalidMemoryType";
+  }
+}
+
+/// Write the given memory map to the file.
+EFI_STATUS SaveMemoryMap(struct MemoryMap *map, EFI_FILE_PROTOCOL *file) {
+  CHAR8 buf[256];
+  UINTN len;
+
+  CHAR8 *header =
+      "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
+  len = AsciiStrLen(header);
+  file->Write(file, &len, header);
+
+  Print(L"map->buffer = 0x%08lx, map->map_size = 0x%08lx\n", map->buffer,
+        map->map_size);
+
+  EFI_PHYSICAL_ADDRESS iter;
+  int i;
+  for (iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i = 0;
+       iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
+       iter += map->descriptor_size, i++) {
+    EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)iter;
+    len = AsciiSPrint(buf, sizeof(buf), "%u, %x, %-ls, %08lx, %lx, %lx\n", i,
+                      desc->Type, GetMemoryTypeUnicode(desc->Type),
+                      desc->PhysicalStart, desc->NumberOfPages,
+                      desc->Attribute & 0xffffflu);
+
+    file->Write(file, &len, buf);
+  }
+
+  return EFI_SUCCESS;
+}
+
+EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root) {
+  EFI_LOADED_IMAGE_PROTOCOL *loaded_image;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs;
+
+  gBS->OpenProtocol(image_handle, &gEfiLoadedImageProtocolGuid,
+                    (VOID **)&loaded_image, image_handle, NULL,
+                    EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  gBS->OpenProtocol(loaded_image->DeviceHandle,
+                    &gEfiSimpleFileSystemProtocolGuid, (VOID **)&fs,
+                    image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  fs->OpenVolume(fs, root);
+
+  return EFI_SUCCESS;
+}
 
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
                            EFI_SYSTEM_TABLE *system_table) {
   Print(L"Hello, world...!\n");
+
+  CHAR8 memory_buf[4096 * 4];
+  struct MemoryMap memmap = {
+      .buffer_size = sizeof(memory_buf),
+      .buffer = memory_buf,
+      .map_size = 0,
+      .map_key = 0,
+      .descriptor_size = 0,
+      .descriptor_version = 0,
+  };
+  GetMemoryMap(&memmap);
+
+  EFI_FILE_PROTOCOL *root_dir;
+  OpenRootDir(image_handle, &root_dir);
+
+  EFI_FILE_PROTOCOL *memmap_file;
+  root_dir->Open(
+      root_dir, &memmap_file, L"\\memmap",
+      EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+
+  SaveMemoryMap(&memmap, memmap_file);
+  memmap_file->Close(memmap_file);
+
+  Print(L"Saved a memory map to \\memmap.\n");
 
   while (1)
     ;
