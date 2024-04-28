b3d07d32b66e12947ec2967249c7068319c9342a
smallkirby <ssmallkirby@gmail.com>
2024-04-28 20:14:37 +0900

    kernel: draw on frame buffer in kernel
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- ZakuroLoaderPkg/Main.c
+++ ZakuroLoaderPkg/Main.c
@@ -1,7 +1,9 @@
 #include <Guid/FileInfo.h>
+#include <Library/MemoryAllocationLib.h>
 #include <Library/PrintLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
+#include <Protocol/BlockIo.h>
 #include <Protocol/BlockIo2.h>
 #include <Protocol/DiskIo.h>
 #include <Protocol/LoadedImage.h>
@@ -116,6 +118,40 @@ EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root) {
   return EFI_SUCCESS;
 }
 
+/// Open Graphics Output Protocol.
+EFI_STATUS OpenGOP(EFI_HANDLE image_handle,
+                   EFI_GRAPHICS_OUTPUT_PROTOCOL **gop) {
+  UINTN num_gop_handles = 0;
+  EFI_HANDLE *gop_handles = NULL;
+  gBS->LocateHandleBuffer(ByProtocol, &gEfiGraphicsOutputProtocolGuid, NULL,
+                          &num_gop_handles, &gop_handles);
+
+  gBS->OpenProtocol(gop_handles[0], &gEfiGraphicsOutputProtocolGuid,
+                    (VOID **)gop, image_handle, NULL,
+                    EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  FreePool(gop_handles);
+
+  return EFI_SUCCESS;
+}
+
+const CHAR16 *GetPixelFormatUnicode(EFI_GRAPHICS_PIXEL_FORMAT fmt) {
+  switch (fmt) {
+    case PixelRedGreenBlueReserved8BitPerColor:
+      return L"PixelRedGreenBlueReserved8BitPerColor";
+    case PixelBlueGreenRedReserved8BitPerColor:
+      return L"PixelBlueGreenRedReserved8BitPerColor";
+    case PixelBitMask:
+      return L"PixelBitMask";
+    case PixelBltOnly:
+      return L"PixelBltOnly";
+    case PixelFormatMax:
+      return L"PixelFormatMax";
+    default:
+      return L"InvalidPixelFormat";
+  }
+}
+
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
                            EFI_SYSTEM_TABLE *system_table) {
   Print(L"Hello, world...!\n");
@@ -144,6 +180,19 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
   memmap_file->Close(memmap_file);
 
   Print(L"Saved a memory map to \\memmap.\n");
+
+  // Open GOP
+  EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
+  OpenGOP(image_handle, &gop);
+  Print(L"Resolution: %ux%u, Pixel Format: %s, %u pixels/line\n",
+        gop->Mode->Info->HorizontalResolution,
+        gop->Mode->Info->VerticalResolution,
+        GetPixelFormatUnicode(gop->Mode->Info->PixelFormat),
+        gop->Mode->Info->PixelsPerScanLine);
+  Print(L"Frame Buffer: 0x%0lx - 0x%0lx, Size: %0lx bytes\n",
+        gop->Mode->FrameBufferBase,
+        gop->Mode->FrameBufferBase + gop->Mode->FrameBufferSize,
+        gop->Mode->FrameBufferSize);
 
   // Load kernel image
   EFI_FILE_PROTOCOL *kernel_file;
@@ -207,8 +256,9 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
     }
   }
 
-  typedef void EntryPointType(void);
-  ((EntryPointType *)entry_addr)();
+  typedef void EntryPointType(UINT64, UINT64);
+  ((EntryPointType *)entry_addr)(gop->Mode->FrameBufferBase,
+                                 gop->Mode->FrameBufferSize);
 
   // unreachable
 
--- kernel/main.zig
+++ kernel/main.zig
@@ -1,4 +1,16 @@
-export fn kernel_main() callconv(.Naked) noreturn {
+//! TODO
+
+/// Kernel entry point called from the bootloader.
+/// The bootloader is a UEFI app using MS x64 calling convention,
+/// so we need to use the same calling convention here.
+export fn kernel_main(
+    frame_buffer_base: [*]u8,
+    frame_buffer_size: u64,
+) callconv(.Win64) noreturn {
+    for (0..frame_buffer_size) |i| {
+        frame_buffer_base[i] = @as(u8, @truncate(i & 0xFF)) % 255;
+    }
+
     while (true) {
         asm volatile ("hlt");
     }
