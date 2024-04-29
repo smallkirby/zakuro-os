f00d010f90cf803066051673613401666109bd49
smallkirby <ssmallkirby@gmail.com>
2024-04-29 20:01:51 +0900

    kernel: move pixel funcs to frame_buffer file
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- kernel/frame_buffer.zig
+++ kernel/frame_buffer.zig
@@ -21,3 +21,35 @@ pub const PixelColor = struct {
     green: u8,
     blue: u8,
 };
+
+/// Get the address of the framebuffer at the specified pixel.
+/// Note that this function does not perform bounds checking.
+pub fn pixel_at(config: *FrameBufferConfig, x: u32, y: u32) [*]u8 {
+    const rel_pos = config.pixels_per_scan_line * y + x;
+    return @ptrCast(&config.frame_buffer[rel_pos * 4]);
+}
+
+/// Write a pixel color to the specified position.
+/// TODO: Can we use a function pointer and struct to avoid repeated switch even in baremetal?
+pub fn write_pixel(config: *FrameBufferConfig, x: u32, y: u32, color: PixelColor) void {
+    switch (config.pixel_format) {
+        .PixelRGBResv8BitPerColor => write_pixel_rgb(config, x, y, color),
+        .PixelBGRResv8BitPerColor => write_pixel_bgr(config, x, y, color),
+    }
+}
+
+/// Write a pixel color to the specified position in RGB format.
+fn write_pixel_rgb(config: *FrameBufferConfig, x: u32, y: u32, color: PixelColor) void {
+    const addr = pixel_at(config, x, y);
+    addr[0] = color.red;
+    addr[1] = color.green;
+    addr[2] = color.blue;
+}
+
+/// Write a pixel color to the specified position in BGR format.
+fn write_pixel_bgr(config: *FrameBufferConfig, x: u32, y: u32, color: PixelColor) void {
+    const addr = pixel_at(config, x, y);
+    addr[0] = color.blue;
+    addr[1] = color.green;
+    addr[2] = color.red;
+}
--- kernel/main.zig
+++ kernel/main.zig
@@ -8,7 +8,7 @@ const fb = @import("frame_buffer.zig");
 export fn kernel_main(fb_config: *fb.FrameBufferConfig) callconv(.Win64) noreturn {
     for (0..fb_config.horizontal_resolution) |x| {
         for (0..fb_config.vertical_resolution) |y| {
-            write_pixel(fb_config, @truncate(x), @truncate(y), .{
+            fb.write_pixel(fb_config, @truncate(x), @truncate(y), .{
                 .red = 0xFF,
                 .green = 0xFF,
                 .blue = 0xFF,
@@ -18,7 +18,7 @@ export fn kernel_main(fb_config: *fb.FrameBufferConfig) callconv(.Win64) noretur
 
     for (0..200) |x| {
         for (0..200) |y| {
-            write_pixel(
+            fb.write_pixel(
                 fb_config,
                 @truncate(x + 100),
                 @truncate(y + 100),
@@ -33,24 +33,5 @@ export fn kernel_main(fb_config: *fb.FrameBufferConfig) callconv(.Win64) noretur
 
     while (true) {
         asm volatile ("hlt");
-    }
-}
-
-/// Draw a pixel at the given position of the framebuffer with the given color.
-fn write_pixel(config: *fb.FrameBufferConfig, x: u32, y: u32, color: fb.PixelColor) void {
-    const pixel_position = config.pixels_per_scan_line * y + x;
-    switch (config.pixel_format) {
-        .PixelRGBResv8BitPerColor => {
-            const p: [*]u8 = @ptrCast(&config.frame_buffer[4 * pixel_position]);
-            p[0] = color.red;
-            p[1] = color.green;
-            p[2] = color.blue;
-        },
-        .PixelBGRResv8BitPerColor => {
-            const p: [*]u8 = @ptrCast(&config.frame_buffer[4 * pixel_position]);
-            p[0] = color.blue;
-            p[1] = color.green;
-            p[2] = color.red;
-        },
     }
 }
