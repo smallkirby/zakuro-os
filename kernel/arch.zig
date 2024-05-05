46826104d153b012ffe25af7e3f76e2b0e5c033b
smallkirby <ssmallkirby@gmail.com>
2024-05-05 10:42:59 +0900

    arch: add a guard for supported archs
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- kernel/arch.zig
+++ kernel/arch.zig
@@ -1,4 +1,8 @@
 //! Thin wrapper to access arch-specific modules.
 //! Now, Zakuro-OS supports only x86_64 architecture.
 
-pub const impl = @import("arch/x86/arch.zig");
+const builtin = @import("builtin");
+pub const impl = switch (builtin.target.cpu.arch) {
+    .x86_64 => @import("arch/x86/arch.zig"),
+    else => @compileError("Unsupported architecture."),
+};
