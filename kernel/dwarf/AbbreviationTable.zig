4a7f997ad84afe510a317bd36444a33cdc255bba
smallkirby <ssmallkirby@gmail.com>
2024-05-20 20:35:16 +0900

    dwarf: license: add license notice
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- kernel/dwarf/AbbreviationTable.zig
+++ kernel/dwarf/AbbreviationTable.zig
@@ -2,6 +2,37 @@
 //! The abbreviations table for a compilation unit consists of a set of abbreviation declarations.
 //! These declarations are used to decode the abbreviation codes in the abbreviation section of the .debug_info section.
 //! The table is parsed from the .debug_abbrev section of an ELF file.
+//!
+//!
+//! ==============================================================================
+//!
+//! LICENCE NOTICE
+//!
+//! The implementation is heavily inspired by https://github.com/kubkon/zig-dwarfdump .
+//! Original LICENCE follows:
+//!
+//! MIT License
+//!
+//! Copyright (c) 2022 Jakub Konka
+//!
+//! Permission is hereby granted, free of charge, to any person obtaining a copy
+//! of this software and associated documentation files (the "Software"), to deal
+//! in the Software without restriction, including without limitation the rights
+//! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//! copies of the Software, and to permit persons to whom the Software is
+//! furnished to do so, subject to the following conditions:
+//!
+//! The above copyright notice and this permission notice shall be included in all
+//! copies or substantial portions of the Software.
+//!
+//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//! SOFTWARE.
+//!
 
 const std = @import("std");
 const leb = std.leb;
