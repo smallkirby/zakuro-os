2146c4a2da5cded1321a4a08f7407f79f4d22ae2
smallkirby <ssmallkirby@gmail.com>
2024-06-23 22:10:33 +0900

    acpi: find FADT by iterating XSDT entries
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- kernel/acpi.zig
+++ kernel/acpi.zig
@@ -1,13 +1,38 @@
 const std = @import("std");
 
+const AcpiError = error{
+    InvalidSignature,
+    InvalidRevision,
+    InvalidChecksum,
+    InvalidExtendedChecksum,
+};
+
+var fadt: ?*Fadt = null;
+
 /// Initialize ACPI.
 pub fn init(rsdp: *Rsdp) void {
     rsdp.valid() catch |e| switch (e) {
-        RsdpError.InvalidSignature => @panic("Invalid RSDP signature."),
-        RsdpError.InvalidRevision => @panic("Invalid RSDP revision."),
-        RsdpError.InvalidChecksum => @panic("Invalid RSDP checksum."),
-        RsdpError.InvalidExtendedChecksum => @panic("Invalid RSDP extended checksum."),
+        AcpiError.InvalidSignature => @panic("Invalid RSDP signature."),
+        AcpiError.InvalidRevision => @panic("Invalid RSDP revision."),
+        AcpiError.InvalidChecksum => @panic("Invalid RSDP checksum."),
+        AcpiError.InvalidExtendedChecksum => @panic("Invalid RSDP extended checksum."),
     };
+
+    const xsdt: *Xsdt = @ptrFromInt(rsdp.xsdt_address);
+    xsdt.header.valid("XSDT") catch |e| switch (e) {
+        AcpiError.InvalidSignature => @panic("Invalid XSDT signature."),
+        AcpiError.InvalidChecksum => @panic("Invalid XSDT checksum."),
+        else => unreachable,
+    };
+
+    var ix: usize = 0;
+    fadt = while (ix < xsdt.size()) : (ix += 1) {
+        const ent = xsdt.get(ix);
+        if (ent.valid("FACP")) |_| {
+            // The signature of FADT is "FACP".
+            break @as(*Fadt, @ptrCast(ent));
+        } else |_| {}
+    } else @panic("FADT not found.");
 }
 
 /// Calculate the checksum of RSDP structure.
@@ -20,14 +45,87 @@ fn checksum(data: []u8) u8 {
     return sum;
 }
 
-const RsdpError = error{
-    InvalidSignature,
-    InvalidRevision,
-    InvalidChecksum,
-    InvalidExtendedChecksum,
+/// ACPI structure that contains information about ACPI fixed registers.
+/// Most of fields are omitted here.
+const Fadt = extern struct {
+    header: DescriptorHeader,
+
+    _reserved1: [76 - @sizeOf(DescriptorHeader)]u8,
+    pm_tmr_blk: u32,
+    _reserved2: [32]u8,
+    flags: u32,
+    _reserved3: [160]u8,
+
+    comptime {
+        if (@sizeOf(Fadt) != 276) {
+            @compileError("Invalid size of FADT.");
+        }
+    }
+};
+
+/// XSDT (Extended System Descriptor Table) structure of ACPI v2.0+..
+/// XSDT starts with a header and followed by a list of 64-bit pointers to other tables.
+const Xsdt = extern struct {
+    header: DescriptorHeader,
+    _pointer_entries: void,
+
+    /// Get the table entry at the specified index.
+    pub fn get(self: *Xsdt, index: usize) *DescriptorHeader {
+        // NOTE: 64bit pointer entries are 4 bytes aligned, not 8 bytes.
+        //   Zig does not allow incorrect alignment cast, for example:
+        //   `const ents: [*]*const DescriptorHeader = @alignCast(@ptrCast(&self._pointer_entries))`
+        //   Therefore, we have to calculate the address from two 32bit integers.
+        const ents_start = @intFromPtr(&self._pointer_entries);
+        const first: *u32 = @ptrFromInt(ents_start + index * @sizeOf(u64));
+        const second: *u32 = @ptrFromInt(ents_start + index * @sizeOf(u64) + @sizeOf(u32));
+        return @ptrFromInt((@as(u64, second.*) << 32) + first.*);
+    }
+
+    /// Number of table entries.
+    pub fn size(self: *Xsdt) usize {
+        return (self.header.length - @sizeOf(DescriptorHeader)) / @sizeOf(u64);
+    }
+
+    comptime {
+        if (@sizeOf(Xsdt) != 36) {
+            @compileError("Invalid size of XSDT.");
+        }
+    }
+};
+
+/// Descriptor header of ACPI structures.
+const DescriptorHeader = extern struct {
+    signature: [4]u8,
+    length: u32,
+    revision: u8,
+    checksum: u8,
+    oem_id: [6]u8,
+    oem_table_id: [8]u8,
+    oem_revision: u32,
+    creator_id: u32,
+    creator_revision: u32,
+
+    _end_marker: void,
+
+    pub fn valid(self: *DescriptorHeader, signature: []const u8) AcpiError!void {
+        if (!std.mem.eql(u8, signature, &self.signature)) {
+            return AcpiError.InvalidSignature;
+        }
+        const ents: [*]u8 = @ptrCast(self);
+        if (checksum(ents[0..self.length]) != 0) {
+            return AcpiError.InvalidChecksum;
+        }
+    }
+
+    comptime {
+        if (@sizeOf(DescriptorHeader) != 36) {
+            @compileError("Invalid size of DescriptorHeader.");
+        }
+    }
 };
 
 /// RSDP (Root System Description Pointer) structure of ACPI v2.0+.
+/// RSDP is used to find XSDT (Extended System Descriptor Table).
 pub const Rsdp = extern struct {
     const size_first_byte = @offsetOf(Rsdp, "length");
     const size_extended_byte = @offsetOf(Rsdp, "_end_marker");
@@ -62,18 +160,25 @@ pub const Rsdp = extern struct {
         }
     }
 
-    fn valid(self: *Rsdp) !void {
+    fn valid(self: *Rsdp) AcpiError!void {
         if (!std.mem.eql(u8, &self.signature, "RSD PTR ")) {
-            return RsdpError.InvalidSignature;
+            return AcpiError.InvalidSignature;
         }
         if (self.revision != 2) {
-            return RsdpError.InvalidRevision;
+            return AcpiError.InvalidRevision;
         }
         if (checksum(std.mem.asBytes(self)[0..size_first_byte]) != 0) {
-            return RsdpError.InvalidChecksum;
+            return AcpiError.InvalidChecksum;
         }
         if (checksum(std.mem.asBytes(self)[0..size_extended_byte]) != 0) {
-            return RsdpError.InvalidExtendedChecksum;
+            return AcpiError.InvalidExtendedChecksum;
         }
     }
 };
+
+test "Size of structures" {
+    std.testing.refAllDecls(@This());
+    try std.testing.expectEqual(36, @sizeOf(DescriptorHeader));
+    try std.testing.expectEqual(36, @sizeOf(Xsdt));
+    try std.testing.expectEqual(276, @sizeOf(Fadt));
+}
